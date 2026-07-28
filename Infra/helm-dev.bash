c9538020ec01499bdc73048bdd3cc6b45a4e8ca9
Andrey <andrey-valeriy.burov@mail.ru>
2026-07-28 14:59:05 +0300

    feat: update helm

--- Infra/helm-dev.bash
+++ Infra/helm-dev.bash
@@ -1,5 +1,13 @@
-  helm upgrade --install bank-dev ./Infra/helm/dev \
-    -f ./Infra/helm/dev/values.yaml \
-    -f ./Infra/helm/dev/values-secret.yaml \
-    --namespace bank-dev \
-    --create-namespace
+kubectl create namespace bank-dev --dry-run=client -o yaml | kubectl apply -f -
+
+kubectl create secret generic jwt-keys \
+  --from-file=private.pem=./Infra/secrets/private.pem \
+  --from-file=public.pem=./Infra/secrets/public.pem \
+  --namespace bank-dev \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+helm upgrade --install bank-dev ./Infra/helm/dev \
+  -f ./Infra/helm/dev/values.yaml \
+  -f ./Infra/helm/dev/values-secret.yaml \
+  --namespace bank-dev \
+  --create-namespace
