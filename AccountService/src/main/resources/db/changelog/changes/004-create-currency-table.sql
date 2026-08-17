f458709e07d68863ffcde8044215b545263ef800
Andrey <andrey-valeriy.burov@mail.ru>
2026-08-17 11:42:36 +0300

    fix: db migration

--- AccountService/src/main/resources/db/changelog/changes/004-create-currency-table.sql
+++ AccountService/src/main/resources/db/changelog/changes/004-create-currency-table.sql
@@ -6,18 +6,17 @@ create table currencies
 (
     id            uuid primary key,
     name          varchar(55) not null unique,
-    rate_from_usd decimal     not null
+    rate_from_usd decimal     not null,
 
     constraint chk_currency_name
         check (name in ('USD', 'EUR', 'CNY', 'GBP'))
 );
 
 insert into currencies (id, name, rate_from_usd)
-values
-    (gen_random_uuid(), 'USD', 1),
-    (gen_random_uuid(), 'EUR', 1),
-    (gen_random_uuid(), 'CNY', 1),
-    (gen_random_uuid(), 'GBP', 1);
+values (gen_random_uuid(), 'USD', 1),
+       (gen_random_uuid(), 'EUR', 1),
+       (gen_random_uuid(), 'CNY', 1),
+       (gen_random_uuid(), 'GBP', 1);
 
 alter table accounts
     drop constraint chk_accounts_currency;
