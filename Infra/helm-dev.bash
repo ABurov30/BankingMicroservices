65e2e501984180926e0a6d83ab35fe1715a307ed
Andrey <andrey-valeriy.burov@mail.ru>
2026-09-19 18:53:02 +0300

    feat: update kuber config

--- Infra/helm-dev.bash
+++ Infra/helm-dev.bash
@@ -1,13 +1,22 @@
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
+
+echo "SCRIPT_DIR:   $SCRIPT_DIR"
+echo "PROJECT_ROOT: $PROJECT_ROOT"
+echo "PWD:          $(pwd)"
+
 kubectl create namespace bank-dev --dry-run=client -o yaml | kubectl apply -f -
 
 kubectl create secret generic jwt-keys \
-  --from-file=private.pem=./Infra/secrets/private.pem \
-  --from-file=public.pem=./Infra/secrets/public.pem \
+  --from-file=private.pem="$PROJECT_ROOT/secrets/private.pem" \
+  --from-file=public.pem="$PROJECT_ROOT/secrets/public.pem" \
   --namespace bank-dev \
   --dry-run=client -o yaml | kubectl apply -f -
 
-helm upgrade --install bank-dev ./Infra/helm/dev \
-  -f ./Infra/helm/dev/values.yaml \
-  -f ./Infra/helm/dev/values-secret.yaml \
+helm upgrade --install bank-dev "$PROJECT_ROOT/Infra/helm/dev" \
+  -f "$PROJECT_ROOT/Infra/helm/dev/values.yaml" \
+  -f "$PROJECT_ROOT/Infra/helm/dev/values-secret.yaml" \
   --namespace bank-dev \
   --create-namespace
+
+kubectl port-forward -n ingress-nginx svc/ingress-nginx-controller 8080:80
